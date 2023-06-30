faf5557214cabff5b62bbc1f0fe50a7e37f8837b
scottyTwoskip <scottyeansor@gmail.com>
2023-06-29 22:47:19 -0700

    fixed the peramaters

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(image, source, left, bottom) {
+    image = document.createElement('img')
+    image.src = source
+    image.style.position = 'fixed'
+    image.style.left = left
+    image.style.bottom = bottom
+    document.body.append(image)
+    return image
+}
+
+
+newImage('greenCharacter', 'assets/green-character.gif', '100px', '100px')
+newImage('pineTree', 'assets/pine-tree.png', '450px', '200px')
+newImage('tree', 'assets/tree.png', '200px', '300px')
+newImage('pillar', 'assets/pillar.png', '350px', '100px')
+newImage('crate', 'assets/crate.png', '150px', '200px')
+newImage('well', 'assets/well.png', '500px', '425px')
+
+
+function newItem(image, source, left, bottom) {
+    let item = newImage(image, source, left, bottom)
+    item.addEventListener("click", function () {
+        item.remove()
+    })
+}
+
+newItem('sword', 'assets/sword.png', '500px', '405px')
+newItem('staff', 'assets/staff.png', '600px', '100px')
+newItem('shield', 'assets/shield.png', '165px', '185px')
+
